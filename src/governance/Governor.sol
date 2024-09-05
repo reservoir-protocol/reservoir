651e382be9590a40e183825b68003e6f03d50bdf
eukadish <eugene.kadish@gmail.com>
2024-09-05 08:12:29 -0400

    cleanup dependencies

--- src/governance/Governor.sol
+++ src/governance/Governor.sol
@@ -2,49 +2,51 @@
 
 pragma solidity ^0.8.0;
 
-import {IERC165} from "openzeppelin-contracts/contracts/interfaces/IERC165.sol";
-
 import {IVotes} from "openzeppelin-contracts/contracts/governance/utils/IVotes.sol";
+import {IGovernor} from "openzeppelin-contracts/contracts/governance/IGovernor.sol";
 
 import {TimelockController} from "openzeppelin-contracts/contracts/governance/TimelockController.sol";
 
-import {IGovernor} from "openzeppelin-contracts/contracts/governance/IGovernor.sol";
-
 import {Governor} from "openzeppelin-contracts/contracts/governance/Governor.sol";
 import {GovernorVotes} from "openzeppelin-contracts/contracts/governance/extensions/GovernorVotes.sol";
-import {GovernorTimelockControl} from "openzeppelin-contracts/contracts/governance/extensions/GovernorTimelockControl.sol";
+import {GovernorSettings} from "openzeppelin-contracts/contracts/governance/extensions/GovernorSettings.sol";
 
-contract ReservoirGovernor is GovernorVotes, GovernorTimelockControl {
+import {GovernorCountingSimple} from "openzeppelin-contracts/contracts/governance/extensions/GovernorCountingSimple.sol";
+import {GovernorTimelockControl} from "openzeppelin-contracts/contracts/governance/extensions/GovernorTimelockControl.sol";
+import {GovernorVotesQuorumFraction} from "openzeppelin-contracts/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+
+contract ReservoirGovernor is
+    Governor,
+    GovernorVotes,
+    GovernorSettings,
+    GovernorCountingSimple,
+    GovernorTimelockControl,
+    GovernorVotesQuorumFraction
+{
     constructor(
         string memory name_,
         IVotes token_,
-        TimelockController timelock_
+        TimelockController timelock_,
+        uint256 votingDelay_,
+        uint256 votingPeriod_,
+        uint256 proposalThreshold_,
+        uint256 quorumNumeratorValue_
     )
-        GovernorVotes(token_)
-        GovernorTimelockControl(timelock_)
         Governor(name_)
+        GovernorVotes(token_)
+        GovernorSettings(votingDelay_, votingPeriod_, proposalThreshold_)
+        GovernorTimelockControl(timelock_)
+        GovernorVotesQuorumFraction(quorumNumeratorValue_)
     {}
 
-    // function quorum(uint256) public pure override returns (uint256) {
-    //     return 0;
-    // }
-
-    // function votingDelay() public pure override returns (uint256) {
-    //     return 4;
-    // }
-
-    // function votingPeriod() public pure override returns (uint256) {
-    //     return 16;
-    // }
-
-    // function cancel(
-    //     address[] memory targets,
-    //     uint256[] memory values,
-    //     bytes[] memory calldatas,
-    //     bytes32 salt
-    // ) public returns (uint256 proposalId) {
-    //     return _cancel(targets, values, calldatas, salt);
-    // }
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
 
     function state(
         uint256 proposalId
