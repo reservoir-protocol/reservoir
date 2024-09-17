ae208d98929f5ee2327672151a63fd72079e416c
eukadish <eugene.kadish@gmail.com>
2024-09-17 08:02:12 -0400

    remove burn functionality

--- src/governance/DAM.sol
+++ src/governance/DAM.sol
@@ -4,15 +4,11 @@ pragma solidity ^0.8.24;
 
 import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-import {SafeCast} from "openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
-
-import {ERC20Burnable} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {ERC20Votes, ERC20, ERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import {Context} from "openzeppelin-contracts/contracts/utils/Context.sol";
 
-contract DAM is AccessControl, ERC20Burnable, ERC20Votes {
+contract DAM is AccessControl, ERC20Votes {
     uint256 private constant supply = 1_000_000_000e18;
 
     bytes32 public constant MINTER = keccak256(abi.encode("dam.minter"));
@@ -32,27 +28,5 @@ contract DAM is AccessControl, ERC20Burnable, ERC20Votes {
     /// @param amount Quantity of token added
     function mint(address account, uint256 amount) external onlyRole(MINTER) {
         _mint(account, amount);
-    }
-
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
-        ERC20Votes._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(
-        address account,
-        uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
-        super._mint(account, amount);
-    }
-
-    function _burn(
-        address account,
-        uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
-        super._burn(account, amount);
     }
 }
